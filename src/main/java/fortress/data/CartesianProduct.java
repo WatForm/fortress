2b54a909c9e9c3d696ab996ea96cd1c8633211d4
Joseph Poremba <josephporemba@gmail.com>
2019-03-13 16:54:52 -0400

    Add cartesian product class to enumerate products of lists
    Will be important for adding range formulas

---
+++ src/main/java/fortress/data/CartesianProduct.java
@@ -0,0 +1,80 @@
+package fortress.data;
+
+import java.util.List;
+import java.lang.Iterable;
+import java.util.Iterator;
+import java.util.ArrayList;
+import fortress.util.Errors;
+import java.lang.UnsupportedOperationException;
+
+public class CartesianProduct<E> implements Iterable<List<E>> {
+    
+    List<List<E>> sets;
+    int numberOfSets;
+    int[] numberOfElements;
+    
+    // Give a list A_1, ... A_n, where each is a list of elements
+    public CartesianProduct(List<List<E>> sets) {
+        this.sets = sets;
+        this.numberOfSets = sets.size();
+        for(List<E> set : sets) {
+            Errors.failIf(set.size() == 0);
+        }
+    }
+    
+    @Override
+    public Iterator iterator() {
+        return new ProductIterator();
+    }
+    
+    public class ProductIterator implements Iterator<List<E>> {
+        int[] currentPosition;
+        boolean atEnd;
+        
+        private ProductIterator() {
+            this.currentPosition = new int[numberOfSets];
+            this.atEnd = false;
+            for(int i = 0; i < currentPosition.length; i++) {
+                this.currentPosition[i] = 0;
+            }
+            this.currentPosition[0] = 0;
+        }
+        
+        @Override
+        public boolean hasNext() {
+            return atEnd == false;
+        }
+        
+        @Override
+        public List<E> next() {
+            Errors.failIf(!hasNext());
+            
+            // Get current item of counter first
+            List<E> productElement = new ArrayList();
+            for(int i = 0; i < numberOfSets; i++) {
+                List<E> set_i = sets.get(i);
+                E element = set_i.get(currentPosition[i]);
+                productElement.add(element);
+            }
+            
+            // Increment
+            int index = 0;
+            while(index < numberOfSets && currentPosition[index] == sets.get(index).size() - 1) {
+                currentPosition[index] = 0;
+                index++;
+            }
+            if(index == numberOfSets) {
+                atEnd = true;
+            } else {
+                currentPosition[index]++;
+            }
+            
+            return productElement;
+        }
+        
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("remove is unsupported");
+        }
+    }
+}
---
+++ src/test/java/data/CartesianProductTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+import org.junit.Ignore;
+
+import fortress.data.CartesianProduct;
+import java.util.List;
+import java.util.Iterator;
+
+public class CartesianProductTest {
+    @Test
+    public void iteration() {
+        List<Integer> l1 = List.of(1, 2, 3);
+        List<Integer> l2 = List.of(4, 5);
+        List<Integer> l3 = List.of(6);
+        List<Integer> l4 = List.of(7, 8);
+        
+        CartesianProduct<Integer> product = new CartesianProduct(List.of(l1, l2, l3, l4));
+        
+        Iterator<List<Integer>> iterator = product.iterator();
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(1, 4, 6, 7), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(2, 4, 6, 7), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(3, 4, 6, 7), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(1, 5, 6, 7), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(2, 5, 6, 7), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(3, 5, 6, 7), iterator.next());
+        assertTrue(iterator.hasNext());
+        
+        assertEquals(List.of(1, 4, 6, 8), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(2, 4, 6, 8), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(3, 4, 6, 8), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(1, 5, 6, 8), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(2, 5, 6, 8), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(List.of(3, 5, 6, 8), iterator.next());
+        assertFalse(iterator.hasNext());
+    }
+}
