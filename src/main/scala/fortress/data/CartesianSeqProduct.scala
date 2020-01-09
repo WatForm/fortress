fde23eeef0a89587d899fa4c90fc3bff972835e3
Joseph Poremba <josephporemba@gmail.com>
2020-01-09 12:49:09 -0500

    Code cleanup of fortress.data package

--- src/main/scala/fortress/data/CartesianSeqProduct.scala
+++ src/main/scala/fortress/data/CartesianSeqProduct.scala
@@ -8,9 +8,15 @@ import scala.collection.immutable.Seq
 
 // TODO I suspect this is an efficiency bottleneck
 
-/** Represents the cartesian product of sequences A_1, ..., A_n.
-    IndexedSeq inputs are required for efficiency. The implementation may be able
-    to be improved and this restriction relaxed. */
+/** Represents the cartesian product of non-empty sequences A_1, ..., A_n, which
+ * can be iterated over.
+ * For example, if given two sequences A_1 = (a, b, c) and A_2 = (d, e, f),
+ * the cartesian product is the sequence ( (a, d), (a, e), (a, f),
+ * (b, d), (b, e), (b, f), (c, d), (c, e), (c, f) ).
+ * The individual elements of the cartesian product are represented by sequences
+ * instead of tuples.
+ * IndexedSeq inputs are required for efficiency. The implementation may be able
+ * to be improved and this restriction relaxed. */
 class CartesianSeqProduct[E](private val sequences: IndexedSeq[IndexedSeq[E]]) extends Iterable[Seq[E]] {
     
     Errors.precondition(sequences.forall(_.nonEmpty))
