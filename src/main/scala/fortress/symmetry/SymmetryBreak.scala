38af5eb9e5b88cb42754951462e7575cab6342dd
Joseph Poremba <josephporemba@gmail.com>
2020-02-28 19:49:33 -0500

    Add helper function and unit test for DRD equalities (symmetry)

--- src/main/scala/fortress/symmetry/SymmetryBreak.scala
+++ src/main/scala/fortress/symmetry/SymmetryBreak.scala
@@ -10,13 +10,15 @@ object Symmetry {
         else OrList(terms)
     }
     
-    def csConstantEqualities(sort: Sort, constants: IndexedSeq[AnnotatedVar],
-        unusedValues: IndexedSeq[DomainElement], usedValues: IndexedSeq[DomainElement]): Set[Term] = {
+    def csConstantEqualities(sort: Sort, constants: IndexedSeq[AnnotatedVar], scope: Int,
+        usedValues: IndexedSeq[DomainElement]): Set[Term] = {
         Errors.precondition(!sort.isBuiltin)
         Errors.precondition(constants.forall(_.sort == sort))
-        Errors.precondition(unusedValues.forall(_.sort == sort))
-        Errors.precondition(unusedValues.forall(_.sort == sort))
-        Errors.precondition((unusedValues intersect usedValues).isEmpty)
+        Errors.precondition(usedValues.forall(_.sort == sort))
+        Errors.precondition(usedValues.forall(_.index <= scope))
+        Errors.precondition(usedValues.size < scope)
+        
+        val unusedValues = (for(i <- 1 to scope) yield DomainElement(i, sort)) diff usedValues
         
         val n = constants.size
         val m = unusedValues.size
@@ -41,13 +43,15 @@ object Symmetry {
         equalityConstraints.toSet
     }
     
-    def csConstantImplications(sort: Sort, constants: IndexedSeq[AnnotatedVar],
-        unusedValues: IndexedSeq[DomainElement], usedValues: IndexedSeq[DomainElement]): Set[Term] = {
+    def csConstantImplications(sort: Sort, constants: IndexedSeq[AnnotatedVar], scope: Int,
+        usedValues: IndexedSeq[DomainElement]): Set[Term] = {
         Errors.precondition(!sort.isBuiltin)
         Errors.precondition(constants.forall(_.sort == sort))
-        Errors.precondition(unusedValues.forall(_.sort == sort))
-        Errors.precondition(unusedValues.forall(_.sort == sort))
-        Errors.precondition((unusedValues intersect usedValues).isEmpty)
+        Errors.precondition(usedValues.forall(_.sort == sort))
+        Errors.precondition(usedValues.forall(_.index <= scope))
+        Errors.precondition(usedValues.size < scope)
+        
+        val unusedValues = (for(i <- 1 to scope) yield DomainElement(i, sort)) diff usedValues
         
         val n = constants.size
         val m = unusedValues.size
@@ -68,5 +72,41 @@ object Symmetry {
         }
         
         implications.toSet
+    }
+    
+    def drdFunctionEqualities(f: FuncDecl, scopes: Map[Sort, Int],
+        usedResultValues: IndexedSeq[DomainElement]): Set[Term]= {
+        Errors.precondition(f.argSorts.forall(!_.isBuiltin))
+        Errors.precondition(!f.resultSort.isBuiltin)
+        Errors.precondition(usedResultValues.forall(_.sort == f.resultSort))
+        Errors.precondition(usedResultValues.forall(_.index <= scopes(f.resultSort)))
+        Errors.precondition(usedResultValues.size < scopes(f.resultSort))
+        
+        val unusedResultValues: IndexedSeq[DomainElement] = (for(i <- 1 to scopes(f.resultSort)) yield DomainElement(i, f.resultSort)) diff usedResultValues
+        
+        val argumentLists = new fortress.util.ArgumentListGenerator(scopes).allArgumentListsOfFunction(f)
+        // For the sake of efficiency, the argument list generator does not generate arguments
+        // until they are needed
+        // Therefore we don't know how many argument tuples there are yet, and have to generate the constraints
+        // imperatively rather than functionally
+        val constraints = new scala.collection.mutable.ListBuffer[Term]
+        var k = 0
+        val argListIterator = argumentLists.iterator
+        while(k < unusedResultValues.size && argListIterator.hasNext) {
+            val argList = argListIterator.next
+            
+            val app = App(f.name, argList)
+            val possibleUsedEqualities =
+                for(v <- usedResultValues) yield {app === v}
+            val possibleUnusedEqualities =
+                for(i <- 0 to k) yield {app === unusedResultValues(i)}
+            val possibleEqualities = possibleUsedEqualities ++ possibleUnusedEqualities
+            
+            constraints += smartOr(possibleEqualities)
+            
+            k += 1
+        }
+        
+        constraints.toSet
     }
 }
--- src/test/scala/symmetry/SymmetryBreakTests.scala
+++ src/test/scala/symmetry/SymmetryBreakTests.scala
@@ -11,6 +11,7 @@ class SymmetryBreakTests extends FunSuite with Matchers {
     
     val A = SortConst("A")
     val B = SortConst("B")
+    val D = SortConst("D")
     
     val c1 = Var("c1")
     val c2 = Var("c2")
@@ -20,7 +21,7 @@ class SymmetryBreakTests extends FunSuite with Matchers {
     
     def DE(index: Int, sort: Sort) = DomainElement(index, sort)
     
-    test("CS Constant Equalities") {
+    test("CS Constant - Equalities") {
         val constants = IndexedSeq(
             c1 of B,
             c2 of B,
@@ -34,14 +35,9 @@ class SymmetryBreakTests extends FunSuite with Matchers {
             DE(5, B),
         )
         
-        val unusedValues = IndexedSeq(
-            DE(2, B),
-            DE(4, B),
-            DE(6, B),
-            DE(7, B)
-        )
+        val scope = 7
         
-        Symmetry.csConstantEqualities(B, constants, unusedValues, usedValues) should be (
+        Symmetry.csConstantEqualities(B, constants, scope, usedValues) should be (
             Set(
                 Or(c1 === DE(1, B), c1 === DE(3, B), c1 === DE(5, B), c1 === DE(2, B)),
                 Or(c2 === DE(1, B), c2 === DE(3, B), c2 === DE(5, B), c2 === DE(2, B), c2 === DE(4, B)),
@@ -51,7 +47,7 @@ class SymmetryBreakTests extends FunSuite with Matchers {
         )
     }
     
-    test("CS Constant Implications") {
+    test("CS Constant - Implications") {
         val constants = IndexedSeq(
             c1 of B,
             c2 of B,
@@ -65,14 +61,9 @@ class SymmetryBreakTests extends FunSuite with Matchers {
             DE(5, B),
         )
         
-        val unusedValues = IndexedSeq(
-            DE(2, B),
-            DE(4, B),
-            DE(6, B),
-            DE(7, B)
-        )
+        val scope = 7
         
-        Symmetry.csConstantImplications(B, constants, unusedValues, usedValues) should be (
+        Symmetry.csConstantImplications(B, constants, scope, usedValues) should be (
             Set(
                 (c2 === DE(4, B)) ==> (c1 === DE(2, B)),
                 (c3 === DE(4, B)) ==> Or(c1 === DE(2, B), c2 === DE(2, B)),
@@ -82,5 +73,59 @@ class SymmetryBreakTests extends FunSuite with Matchers {
                 (c4 === DE(7, B)) ==> Or(c1 === DE(6, B), c2 === DE(6, B), c3 === DE(6, B))
             )
         )
+    }
+    
+    test("DRD Unary - Equalities") {
+        val f = FuncDecl("f", A, B)
+        
+        val usedResultValues = IndexedSeq(
+            DE(2, B),
+            DE(3, B),
+            DE(5, B),
+        )
+        
+        val scopes: Map[Sort, Int] = Map(A -> 8, B -> 7)
+        
+        val f1A = for (i <- Seq(2, 3, 5, 1)) yield {App("f", DE(1, A)) === DE(i, B)}
+        val f2A = for (i <- Seq(2, 3, 5, 1, 4)) yield {App("f", DE(2, A)) === DE(i, B)}
+        val f3A = for (i <- Seq(2, 3, 5, 1, 4, 6)) yield {App("f", DE(3, A)) === DE(i, B)}
+        val f4A = for (i <- Seq(2, 3, 5, 1, 4, 6, 7)) yield {App("f", DE(4, A)) === DE(i, B)}
+        
+        Symmetry.drdFunctionEqualities(f, scopes, usedResultValues) should be (
+            Set(
+                OrList(f1A),
+                OrList(f2A),
+                OrList(f3A),
+                OrList(f4A)
+            )
+        )
+    }
+    
+    test("DRD Ternary - Equalities") {
+        val f = FuncDecl("f", A, D, A, B)
+        
+        val usedResultValues = IndexedSeq(
+            DE(2, B),
+            DE(3, B),
+            DE(5, B),
+        )
+        
+        val scopes: Map[Sort, Int] = Map(A -> 2, D -> 2, B -> 9)
+        
+        val f111 = for (i <- Seq(2, 3, 5, 1)) yield {App("f", DE(1, A), DE(1, D), DE(1, A)) === DE(i, B)}
+        val f211 = for (i <- Seq(2, 3, 5, 1, 4)) yield {App("f", DE(2, A), DE(1, D), DE(1, A)) === DE(i, B)}
+        val f121 = for (i <- Seq(2, 3, 5, 1, 4, 6)) yield {App("f", DE(1, A), DE(2, D), DE(1, A)) === DE(i, B)}
+        val f221 = for (i <- Seq(2, 3, 5, 1, 4, 6, 7)) yield {App("f", DE(2, A), DE(2, D), DE(1, A)) === DE(i, B)}
+        val f112 = for (i <- Seq(2, 3, 5, 1, 4, 6, 7, 8)) yield {App("f", DE(1, A), DE(1, D), DE(2, A)) === DE(i, B)}
+        val f212 = for (i <- Seq(2, 3, 5, 1, 4, 6, 7, 8, 9)) yield {App("f", DE(2, A), DE(1, D), DE(2, A)) === DE(i, B)}
+        
+        val constraints = Symmetry.drdFunctionEqualities(f, scopes, usedResultValues)
+        constraints should have size 6
+        constraints should contain (OrList(f111))
+        constraints should contain (OrList(f211))
+        constraints should contain (OrList(f121))
+        constraints should contain (OrList(f221))
+        constraints should contain (OrList(f112))
+        constraints should contain (OrList(f212))
     }
 }
