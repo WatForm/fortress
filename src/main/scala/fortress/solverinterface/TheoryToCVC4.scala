67f55576dc19a8b1c72f794329887a7b722b7018
Orson Baines <obraines@gmail.com>
2020-06-11 01:40:24 -0600

    Add conversions tables for CVC4 sorts, constants and functions WIP

--- src/main/scala/fortress/solverinterface/TheoryToCVC4.scala
+++ src/main/scala/fortress/solverinterface/TheoryToCVC4.scala
@@ -5,7 +5,13 @@ import edu.stanford.CVC4.{
     Expr => CVC4Expr,
     SmtEngine => CVC4SmtEngine,
     Result => CVC4Result,
-    Kind => CVC4Kind
+    Kind => CVC4Kind,
+    Rational => CVC4Rational,
+    BitVector => CVC4BitVector,
+    vectorExpr => CVC4VectorExpr,
+    vectorType => CVC4VectorType,
+	Type => CVC4Type,
+    FunctionType => CVC4FunctionType
 }
 
 import fortress.msfol._
@@ -18,7 +24,58 @@ class TheoryToCVC4(theory: Theory){
     val em = new CVC4ExprManager
     val smt = new CVC4SmtEngine(em)
 
+    val sortConversionMap: Map[Sort, CVC4Type] = ( for(sort <- theory.sorts) yield
+        (sort, sort match {
+            case SortConst(name) => em mkSort sort.name
+            case BoolSort => em.booleanType
+            case IntSort => em.integerType
+            case BitVectorSort(bitwidth) => em mkBitVectorType bitwidth
+        })
+    ).toMap
+    
+    val constantConversionMap: Map[AnnotatedVar, CVC4Expr] = ( for(cnst <- theory.constants) yield
+        (cnst, em.mkVar(cnst.name, sortConversionMap apply cnst.getSort))
+    ).toMap
+    
+    val funcConversionMap: Map[String, CVC4Expr] = ( for(decl <- theory.functionDeclarations) yield
+        (decl.name, em.mkVar(decl.name, em.mkFunctionType(
+                seqSortToVectorType(decl.argSorts),
+                sortConversionMap.apply(decl.resultSort))))
+    ).toMap
+
     def convert: CVC4SmtEngine = {
         smt
+    }
+
+    def convertExpr(term: Term): CVC4Expr = term match {
+        case Top => em.mkConst(true)
+        case Bottom => em.mkConst(false)
+        case IntegerLiteral(value) => em.mkExpr(CVC4Kind.TO_INTEGER, em.mkConst(new CVC4Rational(value)))
+        case BitVectorLiteral(value, bitwidth) => em.mkConst(new CVC4BitVector(bitwidth, value))
+        case EnumValue(_) | DomainElement(_, _) => Errors.unreachable()
+        case v @ Var(name) => ???
+        case Not(body) => em.mkExpr(CVC4Kind.NOT, convertExpr(body))
+        case AndList(args) => em.mkExpr(CVC4Kind.AND, seqTermToVectorExpr(args))
+        case OrList(args) => em.mkExpr(CVC4Kind.OR, seqTermToVectorExpr(args))
+        case Distinct(args) => em.mkExpr(CVC4Kind.DISTINCT, seqTermToVectorExpr(args))
+        case Implication(left, right) => em.mkExpr(CVC4Kind.IMPLIES, convertExpr(left), convertExpr(right))
+        case Iff(left, right) => em.mkExpr(CVC4Kind.EQUAL, convertExpr(left), convertExpr(right))
+        case Eq(left, right) => em.mkExpr(CVC4Kind.EQUAL, convertExpr(left), convertExpr(right))
+        case App(fname, args) => ???
+        case Exists(vars, body) => ???
+        case Forall(vars, body) => ???
+        case BuiltinApp(fn, args) => ???
+    }
+
+    def seqTermToVectorExpr(seq: Seq[Term]) : CVC4VectorExpr = {
+        val exprs = new CVC4VectorExpr
+        seq.foreach(term => exprs.add(convertExpr(term)))
+        exprs
+    }
+    
+    def seqSortToVectorType(seq: Seq[Sort]) : CVC4VectorType = {
+        val params = new CVC4VectorType
+        seq.foreach(sort => params.add(sortConversionMap.apply(sort)))
+        params
     }
 }
--- src/test/scala/solverinterface/TheoryToCVC4Test.scala
+++ src/test/scala/solverinterface/TheoryToCVC4Test.scala
@@ -2,14 +2,39 @@ import org.scalatest._
 
 import fortress.solverinterface.TheoryToCVC4
 import fortress.msfol._
+import fortress.msfol.Sort._
+import fortress.msfol.Term._
+import fortress.msfol.FuncDecl._
 
 class TheoryToCVC4Test extends UnitSuite {
     test("simple theory test") {
+        val S = mkSortConst("S")
         val theory: Theory = Theory.empty
-            .withAxiom(Term.mkTop)
-        assert(theory != null)
-
+            .withSort(S)
+            .withConstants(mkVar("x") of S, mkVar("y") of S)
+            .withFunctionDeclaration(mkFuncDecl("f", S, S, BoolSort))
+            .withAxiom(mkApp("f", mkVar("x"), mkVar("y")))
+        
         val converter: TheoryToCVC4 = new TheoryToCVC4(theory)
-        assert(converter.convert != null)
+        converter.sortConversionMap.size should be (2)
+        converter.constantConversionMap.size should be (2)
+        converter.funcConversionMap.size should be (1)
+    }
+
+    test("sort conversion") {
+        val theory: Theory = Theory.empty
+            .withSorts(BoolSort, IntSort, BitVectorSort(8), BitVectorSort(16), mkSortConst("S"), mkSortConst("T"))
+    
+        val converter: TheoryToCVC4 = new TheoryToCVC4(theory)
+        val sortMap = converter.sortConversionMap
+    
+        sortMap.size should be (6)
+        // The comparison of types only works with equals, not ==
+        assert(sortMap.apply(BoolSort) equals converter.em.booleanType)
+        assert(sortMap.apply(IntSort) equals converter.em.integerType)
+        assert(sortMap.apply(BitVectorSort(8)) equals converter.em.mkBitVectorType(8))
+        assert(sortMap.apply(BitVectorSort(16)) equals converter.em.mkBitVectorType(16))
+        assert(sortMap.apply(mkSortConst("S")).toString equals "S")
+        assert(sortMap.apply(mkSortConst("T")).toString equals "T")
     }
 }
