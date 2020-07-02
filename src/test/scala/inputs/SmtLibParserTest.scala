e9bff29bf7be41ddf606cf44b99b78eac1eab9cf
Nancy Day <nday@uwaterloo.ca>
2020-07-02 18:49:56 -0400

    adding test for ite

--- src/test/scala/inputs/SmtLibParserTest.scala
+++ src/test/scala/inputs/SmtLibParserTest.scala
@@ -202,7 +202,11 @@ class SmtLibParserTest extends UnitSuite {
         val sf = App("f", x, x)
         val ii = Or(And(y,sf),And(Not(y),y))
         expectedTheory = 
+<<<<<<< HEAD
             expectedTheory.withAxiom(IfThenElse(sf,sf,y)) 
+=======
+            expectedTheory.withAxiom(Or(And(ii,y),And(Not(ii),sf))) 
+>>>>>>> adding test for ite
         resultTheory should be (expectedTheory)
     }
 
