36a09d1adb6e60bcfa6f26f5d10f6a32e14e73ad
Joseph Poremba <josephporemba@gmail.com>
2020-07-02 16:33:33 -0400

    Refactor symmetry breaking transformers to remove duplicate code

---
+++ src/main/scala/fortress/symmetry/SymmetryBreaker.scala
@@ -0,0 +1,79 @@
+package fortress.symmetry
+
+import fortress.msfol._
+import fortress.operations.TermOps._
+import fortress.util.Errors
+
+import scala.collection.mutable
+
+class SymmetryBreaker(theory: Theory, scopes: Map[Sort, Int]) {
+    val tracker = DomainElementTracker.create(theory, scopes)
+    
+    // Accumulates the symmetry breaking constraints
+    val constraints = new mutable.ListBuffer[Term]
+    val newRangeRestrictions = new mutable.ListBuffer[RangeRestriction]
+    
+    def breakConstants(): Unit = {
+        for(sort <- theory.sorts if !sort.isBuiltin && tracker.stillUnusedDomainElements(sort)) {
+            val constants = theory.constants.filter(_.sort == sort).toIndexedSeq
+            val usedVals = tracker.usedDomainElements(sort).toIndexedSeq
+            val scope = scopes(sort)
+            val constantRangeRestrictions = Symmetry.csConstantRangeRestrictions(sort, constants, scope, usedVals)
+            val constantImplications = Symmetry.csConstantImplicationsSimplified(sort, constants, scope, usedVals)
+            
+            // Add to constraints
+            constraints ++= constantRangeRestrictions map (_.asFormula)
+            newRangeRestrictions ++= constantRangeRestrictions
+            constraints ++= constantImplications
+            // Add to used values
+            tracker.markUsed(constantRangeRestrictions flatMap (_.asFormula.domainElements))
+            tracker.markUsed(constantImplications flatMap (_.domainElements))
+        }
+    }
+    
+    def breakFunction(f: FuncDecl): Unit = {
+        val resultSort = f.resultSort
+        val scope = scopes(resultSort)
+        val usedVals = tracker.usedDomainElements(resultSort).toIndexedSeq
+        
+        if(tracker.stillUnusedDomainElements(resultSort)) {
+            if (f.isDomainRangeDistinct) {
+                // DRD scheme
+                val fRangeRestrictions = Symmetry.drdFunctionRangeRestrictions(f, scopes, usedVals)
+                val fImplications = Symmetry.drdFunctionImplicationsSimplified(f, scopes, usedVals)
+                
+                // Add to constraints
+                constraints ++= fRangeRestrictions map (_.asFormula)
+                newRangeRestrictions ++= fRangeRestrictions
+                constraints ++= fImplications
+                // Add to used values
+                tracker.markUsed(fRangeRestrictions flatMap (_.asFormula.domainElements))
+                tracker.markUsed(fImplications flatMap (_.domainElements))
+                
+            } else {
+                // Extended CS scheme
+                val fRangeRestrictions = Symmetry.csFunctionExtRangeRestrictions(f, scope, usedVals)
+                
+                // Add to constraints
+                constraints ++= fRangeRestrictions map (_.asFormula)
+                newRangeRestrictions ++= fRangeRestrictions
+                // Add to used values
+                tracker.markUsed(fRangeRestrictions flatMap (_.asFormula.domainElements))
+            }
+        }
+    }
+    
+    def breakPredicate(P: FuncDecl): Unit = {
+        if(P.argSorts forall (tracker.numUnusedDomainElements(_) >= 2)) { // Need at least 2 unused values to do any symmetry breaking
+            val usedValues: Map[Sort, IndexedSeq[DomainElement]] = tracker.usedDomainElements map {
+                case (sort, setOfVals) => (sort, setOfVals.toIndexedSeq)
+            }
+            val pImplications = Symmetry.predicateImplications(P, scopes, usedValues)
+            
+            // Add to constraints
+            constraints ++= pImplications
+            // Add to used values
+            tracker.markUsed(pImplications flatMap (_.domainElements))
+        }
+    }
+}
--- src/main/scala/fortress/transformers/SymmetryBreakingTransformerONE.scala
+++ src/main/scala/fortress/transformers/SymmetryBreakingTransformerONE.scala
@@ -18,28 +18,9 @@ class SymmetryBreakingTransformerONE extends ProblemStateTransformer {
         
     def apply(problemState: ProblemState): ProblemState = problemState match {
         case ProblemState(theory, scopes, skc, skf, rangeRestricts, unapplyInterp) => {
-            val tracker = DomainElementTracker.create(theory, scopes)
+            val breaker = new SymmetryBreaker(theory, scopes)
             
-            // Accumulates the symmetry breaking constraints
-            val constraints = new mutable.ListBuffer[Term]
-            val newRangeRestrictions = new mutable.ListBuffer[RangeRestriction]
-            
-            // Symmetry break on constants first
-            for(sort <- theory.sorts if !sort.isBuiltin && tracker.stillUnusedDomainElements(sort)) {
-                val constants = theory.constants.filter(_.sort == sort).toIndexedSeq
-                val usedVals = tracker.usedDomainElements(sort).toIndexedSeq
-                val scope = scopes(sort)
-                val constantRangeRestrictions = Symmetry.csConstantRangeRestrictions(sort, constants, scope, usedVals)
-                val constantImplications = Symmetry.csConstantImplications(sort, constants, scope, usedVals)
-                
-                // Add to constraints
-                constraints ++= constantRangeRestrictions map (_.asFormula)
-                newRangeRestrictions ++= constantRangeRestrictions
-                constraints ++= constantImplications
-                // Add to used values
-                tracker.markUsed(constantRangeRestrictions flatMap (_.asFormula.domainElements))
-                tracker.markUsed(constantImplications flatMap (_.domainElements))
-            }
+            breaker.breakConstants()
             
             // After constants, do functions
             
@@ -50,22 +31,11 @@ class SymmetryBreakingTransformerONE extends ProblemStateTransformer {
             // Even arity doesn't make a difference, since it all boils
             // down to symmetry breaking a unary function
             for(f <- theory.functionDeclarations if isAtoA(f)) {
-                val sort = f.resultSort
-                val scope = scopes(sort)
-                val usedVals = tracker.usedDomainElements(sort).toIndexedSeq
-                if(tracker.stillUnusedDomainElements(sort)) {
-                    val fRangeRestrictions = Symmetry.csFunctionExtRangeRestrictions(f, scope, usedVals)
-                    
-                    // Add to constraints
-                    constraints ++= fRangeRestrictions map (_.asFormula)
-                    newRangeRestrictions ++= fRangeRestrictions
-                    // Add to used values
-                    tracker.markUsed(fRangeRestrictions flatMap (_.asFormula.domainElements))
-                }
+                breaker.breakFunction(f)
             }
             
-            val newTheory = theory.withAxioms(constraints.toList)
-            ProblemState(newTheory, scopes, skc, skf, rangeRestricts union newRangeRestrictions.toSet, unapplyInterp)
+            val newTheory = theory.withAxioms(breaker.constraints.toList)
+            ProblemState(newTheory, scopes, skc, skf, rangeRestricts union breaker.newRangeRestrictions.toSet, unapplyInterp)
         }
     }
     
