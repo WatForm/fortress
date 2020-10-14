7f7be1253a246b62bf2cab19277265be9057345c
Khadija Tariq <khadija.tariq@uwaterloo.ca>
2020-10-13 22:52:27 -0400

    Maximizing number of passes to learn literals

--- src/main/scala/fortress/transformers/SimplifyTransformer.scala
+++ src/main/scala/fortress/transformers/SimplifyTransformer.scala
@@ -10,7 +10,7 @@ class SimplifyTransformer extends TheoryTransformer {
     
     override def apply(theory: Theory): Theory =  {
         var learnedLiterals : Map[Term,LeafTerm] = Map.empty
-        val newAxioms = theory.axioms.map(axiom => {
+        var newAxioms = theory.axioms.map(axiom => {
             val newAxiom = axiom.simplify(learnedLiterals)
             newAxiom match {
                 case Not(Eq(t1, t2)) => learnedLiterals = learnedLiterals + (Eq(t1, t2) -> Bottom)
@@ -20,6 +20,23 @@ class SimplifyTransformer extends TheoryTransformer {
             }
             newAxiom
         }).filter(_ != Top)
+        var numLiterals = 0
+        while (numLiterals != learnedLiterals.size) {
+            numLiterals = learnedLiterals.size
+            newAxioms = newAxioms.map(axiom => axiom match {
+                case App(_, _) | Eq(_, _) | Not(App(_, _)) | Not(Eq(_, _)) => axiom
+                case _ => {
+                    val newAxiom = axiom.simplify(learnedLiterals)
+                    newAxiom match {
+                        case Not(Eq(t1, t2)) => learnedLiterals = learnedLiterals + (Eq(t1, t2) -> Bottom)
+                        case Not(App(fname, args)) => learnedLiterals = learnedLiterals + (App(fname, args) -> Bottom)
+                        case App(_, _) | Eq(_, _) => learnedLiterals = learnedLiterals + (newAxiom -> Top)
+                        case _ =>
+                    }
+                    newAxiom
+                }
+            }).filter(_ != Top)
+        }
         Theory(theory.signature, newAxioms)
     }
     
