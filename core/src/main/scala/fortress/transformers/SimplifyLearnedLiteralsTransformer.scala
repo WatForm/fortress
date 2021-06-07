89f1bafd4743d5e9ac3d1b74f4bdeab2325a4ce3
RuomeiYan <r35yan@uwaterloo.ca>
2021-06-07 15:19:04 +0800

    Separated learned literals to a new model finder

---
+++ core/src/main/scala/fortress/transformers/SimplifyLearnedLiteralsTransformer.scala
@@ -0,0 +1,45 @@
+package fortress.transformers
+
+import fortress.msfol._
+import fortress.operations.TermOps._
+
+/** Applies a simplification to the formulas in a theory, replacing them with equivalent formulas.
+  * All other aspects of the theory remain unchanged. */
+class SimplifyLearnedLiteralsTransformer extends TheoryTransformer {
+    
+    override def apply(theory: Theory): Theory =  {
+        var learnedLiterals : Map[Term,LeafTerm] = Map.empty
+        var newAxioms = theory.axioms.map(axiom => {
+            val newAxiom = axiom.simplifyWithLearnedLiterals(learnedLiterals)
+            newAxiom match {
+                case Not(Eq(t1, t2)) => learnedLiterals = learnedLiterals + (Eq(t1, t2) -> Bottom) + (Eq(t2, t1) -> Bottom)
+                case Not(App(fname, args)) => learnedLiterals = learnedLiterals + (App(fname, args) -> Bottom)
+                case Eq(t1, t2) => learnedLiterals = learnedLiterals + (newAxiom -> Top) + (Eq(t2, t1) -> Top)
+                case App(_, _) => learnedLiterals = learnedLiterals + (newAxiom -> Top)
+                case _ =>
+            }
+            newAxiom
+        }).filter(_ != Top)
+        var numLiterals = 0
+        while (numLiterals != learnedLiterals.size) {
+            numLiterals = learnedLiterals.size
+            newAxioms = newAxioms.map(axiom => axiom match {
+                case App(_, _) | Eq(_, _) | Not(App(_, _)) | Not(Eq(_, _)) => axiom
+                case _ => {
+                    val newAxiom = axiom.simplifyWithLearnedLiterals(learnedLiterals)
+                    newAxiom match {
+                        case Not(Eq(t1, t2)) => learnedLiterals = learnedLiterals + (Eq(t1, t2) -> Bottom) + (Eq(t2, t1) -> Bottom)
+                        case Not(App(fname, args)) => learnedLiterals = learnedLiterals + (App(fname, args) -> Bottom)
+                        case Eq(t1, t2) => learnedLiterals = learnedLiterals + (newAxiom -> Top) + (Eq(t2, t1) -> Top)
+                        case App(_, _)  => learnedLiterals = learnedLiterals + (newAxiom -> Top)
+                        case _ =>
+                    }
+                    newAxiom
+                }
+            }).filter(_ != Top)
+        }
+        Theory(theory.signature, newAxioms)
+    }
+
+    override def name: String = "Simplify Transformer"
+}
--- core/src/test/scala/transformers/SimplifyTransformerTest.scala
+++ core/src/test/scala/transformers/SimplifyTransformerTest.scala
@@ -3,7 +3,7 @@ import org.scalatest._
 import fortress.msfol._
 import fortress.transformers._
 
-class SimplifyTransformerTest extends UnitSuite  {
+class SimplifyTransformerTest extends UnitSuite {
 
     val st = new SimplifyTransformer
 
@@ -16,60 +16,21 @@ class SimplifyTransformerTest extends UnitSuite  {
     val P = FuncDecl.mkFuncDecl("P", A, Sort.Bool)
 
     val baseTheory = Theory.empty
-        .withSort(A)
-        .withConstant(x.of(A))
-        .withConstant(y.of(A))
-        .withFunctionDeclaration(f)
-        .withFunctionDeclaration(P)
+      .withSort(A)
+      .withConstant(x.of(A))
+      .withConstant(y.of(A))
+      .withFunctionDeclaration(f)
+      .withFunctionDeclaration(P)
 
     test("basic simplify") {
-        pending
-    }
-
-    test("positive literal equals") {
         val theory = baseTheory
-            .withAxiom(Eq(x, App("f", y)))
-            .withAxiom(Or(Eq(x, App("f", y)), App("P", y)))
+          .withAxiom(Not(Not(Eq(x, App("f", y)))))
+          .withAxiom(Or(Eq(x, App("f", y)), App("P", y), Top) <==> Top)
 
         val expected = baseTheory
-            .withAxiom(Eq(x, App("f", y)))
+          .withAxiom(Eq(x, App("f", y)))
+          .withAxiom(Top)
 
-        st(theory) should be (expected)
-    }
-
-    test("negative literal equals") {
-        val theory = baseTheory
-            .withAxiom(Not(Eq(x, App("f", y))))
-            .withAxiom(And(Eq(x, App("f", y)), App("P", y)))
-
-        val expected = baseTheory
-            .withAxiom(Not(Eq(x, App("f", y))))
-            .withAxiom(Bottom)
-
-        st(theory) should be (expected)
-    }
-
-    test("positive literal app") {
-        val theory = baseTheory
-            .withAxiom(App("P", y))
-            .withAxiom(And(Eq(x, App("f", y)), App("P", y)))
-
-        val expected = baseTheory
-            .withAxiom(App("P", y))
-            .withAxiom(Eq(x, App("f", y)))
-
-        st(theory) should be (expected)
-    }
-
-    test("negative literal app") {
-        val theory = baseTheory
-            .withAxiom(Not(App("P", y)))
-            .withAxiom(Or(Eq(x, App("f", y)), App("P", y)))
-
-        val expected = baseTheory
-            .withAxiom(Not(App("P", y)))
-            .withAxiom(Eq(x, App("f", y)))
-
-        st(theory) should be (expected)
+        st(theory) should be(expected)
     }
 }
