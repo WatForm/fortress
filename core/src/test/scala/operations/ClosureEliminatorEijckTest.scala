6946a31fb65fb319dca34b838e23e048c47ffceb
Owen Zila <otz.zila@gmail.com>
2022-04-25 22:05:34 -0400

    Beginning work on tests

---
+++ core/src/test/scala/operations/ClosureEliminatorEijckTest.scala
@@ -0,0 +1,9 @@
+package operations
+
+import org.scalatest.flatspec.AnyFlatSpec
+import fortress.operations.ClosureEliminatorEijck
+
+class ClosureEliminatorEijckTest extends AnyFlatSpec {
+    
+
+}
