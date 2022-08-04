1510d3f78c62f10f5175338d4250e6be17b99fcd
zxt539 <zhouxintong2000@163.com>
2022-08-04 10:45:04 -0400

    2022/8/4

--- examples/Pigeonhole.java
+++ examples/Pigeonhole.java
@@ -58,7 +58,6 @@ public class Pigeonhole {
             // Set the scopes of the model finder
             finder.setAnalysisScope(P, numPigeons, false);
             finder.setAnalysisScope(H, numHoles, true);
-            
             // Check if all axioms in the theory are satisfiable
             ModelFinderResult result = finder.checkSat();
             
