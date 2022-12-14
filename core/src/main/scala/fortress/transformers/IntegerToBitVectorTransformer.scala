a529d83fc895ae41fe58ee2a5933ee58cbc6f633
Owen Zila <otz.zila@gmail.com>
2022-12-14 16:56:22 -0500

    Changed IntegerToBitvector to include a scope
    This scope expects integers to give the number of values in the scope.
    NOT the number of bits to use!!

--- core/src/main/scala/fortress/transformers/IntegerToBitVectorTransformer.scala
+++ core/src/main/scala/fortress/transformers/IntegerToBitVectorTransformer.scala
@@ -6,20 +6,27 @@ import fortress.operations.LiaChecker
 import fortress.util.Errors
 import fortress.operations.TermOps._
 import fortress.problemstate.ProblemState
+import fortress.problemstate.ExactScope
+import fortress.problemstate.Scope
 
 /** Replaces integers with bitvectors of the given bitwidth. */
 object IntegerToBitVectorTransformer extends ProblemStateTransformer {
     
     override def apply(problemState: ProblemState): ProblemState = {
         if (problemState.scopes.contains(IntSort)) {
-            val bitwidth = problemState.scopes(IntSort)  // Scope
+            val numValues = problemState.scopes(IntSort).size
+            // Do log_2 of numValues to find bitwidth
+            val bitwidth: Int = math.round(math.ceil(math.log(numValues) / math.log(2))).toInt
+            // We may store more values than initially given
+            val totalValues = math.pow(2, bitwidth).toInt
+
             val newAxioms = problemState.theory.axioms.map( axiom => {
-                if( !axiom.isLia ) axiom.intToBitVector(bitwidth.size)
+                if( !axiom.isLia ) axiom.intToBitVector(bitwidth)
                 else axiom
             })
-            val newSig = problemState.theory.signature.replaceIntegersWithBitVectors(bitwidth.size)
+            val newSig = problemState.theory.signature.replaceIntegersWithBitVectors(bitwidth)
             // remove IntSort from the scopes map
-            val newScopes = problemState.scopes.filter(x => !(x._1 == IntSort))
+            val newScopes: Map[Sort, Scope] = problemState.scopes.filter(x => !(x._1 == IntSort)) + (BitVectorSort(bitwidth) -> ExactScope(totalValues))
             // this is the return value
             problemState.withTheory(Theory.mkTheoryWithSignature(newSig).withAxioms(newAxioms)).withScopes(newScopes)
 
--- core/src/test/scala/transformers/IntegerToBitVectorTransformerTest.scala
+++ core/src/test/scala/transformers/IntegerToBitVectorTransformerTest.scala
@@ -8,13 +8,13 @@ class IntegerToBitVectorTransformerTest extends UnitSuite {
         val theory = Theory.empty
             .withAxiom(Not(IntegerLiteral(1) === IntegerLiteral(2)))
 
-        val problemState = ProblemState(theory,Map(IntSort -> ExactScope(4)))
+        val problemState = ProblemState(theory,Map(IntSort -> ExactScope(16)))
 
         val expected = ProblemState(
             Theory.empty
             .withAxiom(Not(
                 BitVectorLiteral(value = 1, bitwidth = 4) === BitVectorLiteral(value = 2, bitwidth = 4))),
-            Map.empty
+            Map.empty + (BitVectorSort(4) -> ExactScope(16))
         )
 
         val transformer = IntegerToBitVectorTransformer
@@ -28,13 +28,13 @@ class IntegerToBitVectorTransformerTest extends UnitSuite {
             .withConstant(i of IntSort)
             .withAxiom(i === IntegerLiteral(5))
 
-        val problemState = ProblemState(theory,Map(IntSort->ExactScope(4)))
+        val problemState = ProblemState(theory,Map(IntSort->ExactScope(16)))
 
         val expected = ProblemState(
             Theory.empty
             .withConstant(i of BitVectorSort(4))
             .withAxiom(i === BitVectorLiteral(value = 5, bitwidth = 4)),
-            Map.empty
+            Map.empty + (BitVectorSort(4) -> ExactScope(16))
         )
         
         val transformer = IntegerToBitVectorTransformer
@@ -49,13 +49,13 @@ class IntegerToBitVectorTransformerTest extends UnitSuite {
             .withConstants(i of IntSort, j of IntSort)
             .withAxiom(BuiltinApp(IntPlus, i, j) === BuiltinApp(IntPlus, i, j))
 
-        val problemState = ProblemState(theory,Map(IntSort->ExactScope(4)))
+        val problemState = ProblemState(theory,Map(IntSort->ExactScope(16)))
         
         val expected = ProblemState(
             Theory.empty
             .withConstants(i of BitVectorSort(4), j of BitVectorSort(4))
             .withAxiom(BuiltinApp(BvPlus, i, j) === BuiltinApp(BvPlus, i, j)),
-            Map.empty
+            Map.empty + (BitVectorSort(4) -> ExactScope(16))
         )
 
         val transformer = IntegerToBitVectorTransformer
@@ -72,7 +72,7 @@ class IntegerToBitVectorTransformerTest extends UnitSuite {
             .withConstant(c of A)
             .withAxiom(App("f", c, IntegerLiteral(5)) === IntegerLiteral(7))
 
-        val problemState = ProblemState(theory,Map(IntSort->ExactScope(4)))
+        val problemState = ProblemState(theory,Map(IntSort->ExactScope(16)))
         
         val expected = ProblemState(
             Theory.empty
@@ -80,7 +80,7 @@ class IntegerToBitVectorTransformerTest extends UnitSuite {
             .withFunctionDeclaration(FuncDecl("f", A, BitVectorSort(4), BitVectorSort(4)))
             .withConstant(c of A)
             .withAxiom(App("f", c, BitVectorLiteral(value = 5, bitwidth = 4)) === BitVectorLiteral(value = 7, bitwidth = 4)),
-            Map.empty
+            Map.empty + (BitVectorSort(4) -> ExactScope(16))
         )
 
         val transformer = IntegerToBitVectorTransformer
@@ -95,13 +95,13 @@ class IntegerToBitVectorTransformerTest extends UnitSuite {
             .withFunctionDeclaration(FuncDecl("f", IntSort, IntSort, IntSort))
             .withAxiom(Forall( Seq(x of IntSort, y of IntSort), BuiltinApp(IntPlus, x, y) === App("f", x, y)))
 
-        val problemState = ProblemState(theory,Map(IntSort->ExactScope(4)))
+        val problemState = ProblemState(theory,Map(IntSort->ExactScope(16)))
         
         val expected = ProblemState(
             Theory.empty
             .withFunctionDeclaration(FuncDecl("f", BitVectorSort(4), BitVectorSort(4), BitVectorSort(4)))
             .withAxiom(Forall( Seq(x of BitVectorSort(4), y of BitVectorSort(4)), BuiltinApp(BvPlus, x, y) === App("f", x, y))),
-            Map.empty
+            Map.empty + (BitVectorSort(4) -> ExactScope(16))
         )
 
         val transformer = IntegerToBitVectorTransformer
@@ -123,7 +123,7 @@ class IntegerToBitVectorTransformerTest extends UnitSuite {
                 .withAxiom(Forall( Seq(x of IntSort, y of IntSort), BuiltinApp(IntPlus, x, y) === App("f", x, y)))
                 .withAxiom(axiom1)
 
-        val problemState = ProblemState(theory,Map(IntSort->ExactScope(4)))
+        val problemState = ProblemState(theory,Map(IntSort->ExactScope(16)))
 
         val expected = ProblemState(
             Theory.empty
@@ -131,7 +131,7 @@ class IntegerToBitVectorTransformerTest extends UnitSuite {
                     .withFunctionDeclaration(FuncDecl("g", UnBoundedIntSort, UnBoundedIntSort, UnBoundedIntSort))
                     .withAxiom(Forall( Seq(x of BitVectorSort(4), y of BitVectorSort(4)), BuiltinApp(BvPlus, x, y) === App("f", x, y)))
                     .withAxiom(axiom1),
-                    Map.empty
+                    Map.empty + (BitVectorSort(4) -> ExactScope(16))
         )
 
         val transformer = IntegerToBitVectorTransformer
