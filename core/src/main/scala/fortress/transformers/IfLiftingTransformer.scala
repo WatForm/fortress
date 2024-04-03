1b4e3f155c0d53bfc24c1d9046bcf8347c7675fc
Nancy Day <nday@uwaterloo.ca>
2024-04-03 14:28:15 -0400

    added check that problem has ites before doing iflifting

--- core/src/main/scala/fortress/transformers/IfLiftingTransformer.scala
+++ core/src/main/scala/fortress/transformers/IfLiftingTransformer.scala
@@ -10,26 +10,29 @@ import fortress.problemstate.ProblemState
 object IfLiftingTransformer extends ProblemStateTransformer {
     override def apply(problemState: ProblemState): ProblemState = {
 
-
-        
-        val theory = problemState.theory
-        var newTheory = theory.mapAxioms(_.iflift)
-        // We only remove a definition before readding it so all its dependencies are in the sig
-        for(cDef <- theory.signature.constantDefinitions){
-            newTheory = newTheory.withoutConstantDefinition(cDef)
-            newTheory = newTheory.withConstantDefinition(cDef.mapBody(_.iflift))
-        }
-        for(fDef <- theory.signature.functionDefinitions){
-            newTheory = newTheory.withoutFunctionDefinition(fDef)
-            newTheory = newTheory.withFunctionDefinition(fDef.mapBody(_.iflift))
-        }
-        
-        problemState.copy(
-            theory = newTheory,
-            flags = problemState.flags.copy(
-                containsItes = false
+        if (problemState.flags.containsItes) {
+            
+            val theory = problemState.theory
+            var newTheory = theory.mapAxioms(_.iflift)
+            // We only remove a definition before readding it so all its dependencies are in the sig
+            for(cDef <- theory.signature.constantDefinitions){
+                newTheory = newTheory.withoutConstantDefinition(cDef)
+                newTheory = newTheory.withConstantDefinition(cDef.mapBody(_.iflift))
+            }
+            for(fDef <- theory.signature.functionDefinitions){
+                newTheory = newTheory.withoutFunctionDefinition(fDef)
+                newTheory = newTheory.withFunctionDefinition(fDef.mapBody(_.iflift))
+            }
+            
+            return problemState.copy(
+                theory = newTheory,
+                flags = problemState.flags.copy(
+                    containsItes = false
+                )
             )
-        )
+        } else {
+            return problemState
+        }
     }
     
     override def name: String = "IfLifting Transformer"
