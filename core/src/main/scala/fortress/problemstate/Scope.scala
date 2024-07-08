11bd6c19d75250f7c71efc067f5c2117ebe74617
Nancy Day <nday@uwaterloo.ca>
2024-07-08 13:50:59 -0400

    function to make a scope fixed

--- core/src/main/scala/fortress/problemstate/Scope.scala
+++ core/src/main/scala/fortress/problemstate/Scope.scala
@@ -3,25 +3,31 @@ package fortress.problemstate
 import fortress.util.Errors
 
 sealed trait Scope {
-    def isExact: Boolean // If false solution can use up to size elements
+    def isExact: Boolean // If false scope can have up to size elements
     def size: Int
-    def isUnchanging: Boolean // For dumping only (currently) the problem cannot be attempted at different scope sizes
+    def isUnchanging: Boolean // the problem cannot be attempted at different scope sizes
 }
 
 case class ExactScope(size: Int, isUnchanging: Boolean) extends Scope {
     Errors.Internal.precondition(size > 0, "ExactScope size must be positive")
     def isExact = true
+    def mkUnchanging(): ExactScope =
+        new ExactScope(this.size, true)
+
 }
 object ExactScope{
     def apply(size: Int) = new ExactScope(size, false)
-    def apply(size: Int, fixed: Boolean) = new ExactScope(size, fixed)
+    def apply(size: Int, isUnchanging: Boolean) = new ExactScope(size, isUnchanging)
 }
 
 case class NonExactScope(size: Int, isUnchanging: Boolean) extends Scope {
     Errors.Internal.precondition(size > 0, "NonExactScope size must be positive")
     def isExact = false
+
+    def mkUnchanging(): NonExactScope =
+        new NonExactScope(this.size, true)
 }
 object NonExactScope{
     def apply(size: Int) = new NonExactScope(size, false)
-    def apply(size: Int, fixed: Boolean) = new NonExactScope(size, fixed)
+    def apply(size: Int, isUnchanging: Boolean) = new NonExactScope(size, isUnchanging)
 }
