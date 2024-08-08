3f08efb8300c8030e4068c13bcbe220030737caf
Ryan Dancy <ryan@keal.ca>
2024-08-07 22:29:14 -0400

    Explicitly use a shallow evaluate

--- core/src/main/scala/fortress/operations/Evaluator.scala
+++ core/src/main/scala/fortress/operations/Evaluator.scala
@@ -12,10 +12,27 @@ class Evaluator(theory: Theory) {
 
     private val defnCache = mutable.Map[(String, Seq[Option[Value]]), Option[Value]]()
 
-    def evaluate(term: Term): Option[Value] = doEvaluate(term, Map.empty)
+    /** Evaluate the term fully to a value if possible. */
+    def evaluate(term: Term): Option[Value] = fullEvaluate(term, Map.empty)
+
+    /** Evaluate the term, but do not recurse through arguments beyond checking whether they are a value. */
+    def shallowEvaluate(term: Term): Option[Value] = {
+        def tryCastToValue(term: Term): Option[Value] = term match {
+            case value: Value => Some(value)
+            case _ => None
+        }
+        doEvaluate(term, Map.empty, tryCastToValue)
+    }
+
+    private def fullEvaluate(term: Term, varMap: Map[Var, Option[Value]]): Option[Value] = {
+        def subEvaluate(term: Term): Option[Value] = doEvaluate(term, varMap, subEvaluate)
+        subEvaluate(term)
+    }
 
     // Get the value that term evaluates to, or None if term is not interpretation-independent.
-    private def doEvaluate(term: Term, varMap: Map[Var, Option[Value]]): Option[Value] = term match {
+    // recurse is the function to use for recursing on arguments.
+    private def doEvaluate(term: Term, varMap: Map[Var, Option[Value]], recurse: Term => Option[Value]): Option[Value] =
+        term match {
         // Value literals
         case Top => Some(Top)
         case Bottom => Some(Bottom)
@@ -29,13 +46,13 @@ class Evaluator(theory: Theory) {
             // Evaluate each argument and evaluate the body with a new variable context.
             // Note that we still evaluate even if not all of the arguments evaluate; they could be short-circuited out.
             case Some(fDef) =>
-                val newArgs = args map (doEvaluate(_, varMap))
+                val newArgs = args map recurse
                 defnCache.getOrElseUpdate((name, newArgs),
-                    doEvaluate(fDef.body, ((fDef.argSortedVar map (_.variable)) zip newArgs).toMap))
+                    fullEvaluate(fDef.body, ((fDef.argSortedVar map (_.variable)) zip newArgs).toMap))
         }
 
         // Evaluate boolean operations. This corresponds to 3-valued logic with an unknown.
-        case Not(p) => doEvaluate(p, varMap) map {
+        case Not(p) => recurse(p) map {
             case Top => Bottom
             case Bottom => Top
         }
@@ -43,7 +60,7 @@ class Evaluator(theory: Theory) {
             def reduceAnd(args: Seq[Term]): Option[Value] = args match {
                 // Reduce with short-circuiting: don't even evaluate if we've short-circuited
                 case Seq() => Some(Top)
-                case head +: tail => doEvaluate(head, varMap) match {
+                case head +: tail => recurse(head) match {
                     case Some(Bottom) => Some(Bottom) // short-circuit: false && x == false for all x
                     case Some(Top) => reduceAnd(tail) // true && x == x for all x
                     case None => reduceAnd(tail) flatMap {
@@ -57,7 +74,7 @@ class Evaluator(theory: Theory) {
             def reduceOr(args: Seq[Term]): Option[Value] = args match {
                 // Reduce with short-circuiting: don't even evaluate if we've short-circuited
                 case Seq() => Some(Bottom)
-                case head +: tail => doEvaluate(head, varMap) match {
+                case head +: tail => recurse(head) match {
                     case Some(Top) => Some(Top) // short-circuit: true || x == true for all x
                     case Some(Bottom) => reduceOr(tail) // false || x == x for all x
                     case None => reduceOr(tail) flatMap {
@@ -71,7 +88,7 @@ class Evaluator(theory: Theory) {
             def reduceDistinct(args: Seq[Term], seen: Set[Value]): Option[Value] = args match {
                 // Reduce with short-circuiting again: short-circuit if the evaluated value is already seen
                 case Seq() => Some(Top)
-                case head +: tail => doEvaluate(head, varMap) match {
+                case head +: tail => recurse(head) match {
                     case Some(value) =>
                         if (seen contains value) Some(Bottom) // short-circuit: two are equal
                         else reduceDistinct(tail, seen + value)
@@ -84,26 +101,26 @@ class Evaluator(theory: Theory) {
             reduceDistinct(args, Set.empty)
         case Implication(left, right) =>
             // Short-circuit: don't evaluate right if left is unknown or false
-            doEvaluate(left, varMap) flatMap {
-                case Top => doEvaluate(right, varMap)
+            recurse(left) flatMap {
+                case Top => recurse(right)
                 case Bottom => Some(Top) // (false => x) == true for all x
             }
-        case Iff(left, right) => (doEvaluate(left, varMap) zip doEvaluate(right, varMap)) map {
+        case Iff(left, right) => (recurse(left) zip recurse(right)) map {
             case (a, b) => fromBool(toBool(a) == toBool(b))
         }
-        case Eq(left, right) => (doEvaluate(left, varMap) zip doEvaluate(right, varMap)) map {
+        case Eq(left, right) => (recurse(left) zip recurse(right)) map {
             case (a, b) => fromBool(a == b)
         }
         case IfThenElse(condition, ifTrue, ifFalse) =>
             // Short-circuit: if condition evaluates, don't even evaluate the other branch
-            doEvaluate(condition, varMap) flatMap {
-                case Top => doEvaluate(ifTrue, varMap)
-                case Bottom => doEvaluate(ifFalse, varMap)
+            recurse(condition) flatMap {
+                case Top => recurse(ifTrue)
+                case Bottom => recurse(ifFalse)
             }
 
         // TODO: Evaluate quantifiers by expanding?
-        case Forall(vars, body) => None
-        case Exists(vars, body) => None
+        case Forall(_, _) => None
+        case Exists(_, _) => None
 
         // TODO: Evaluate closures. (Currently, run this after closure elimination).
         case Closure(_, _, _, _) => None
