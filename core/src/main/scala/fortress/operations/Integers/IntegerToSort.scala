c729ee57ed8f2738e1aedaeb276fd3d157d70d5f
Owen Zila <otz.zila@gmail.com>
2025-07-29 21:11:16 -0400

    Integer to Sort without signature changes

--- core/src/main/scala/fortress/operations/Integers/IntegerToSort.scala
+++ core/src/main/scala/fortress/operations/Integers/IntegerToSort.scala
@@ -5,10 +5,8 @@ import fortress.util.Errors
 import fortress.data.NameGenerator
 
 class IntegerToSortConverter(min: Int, max: Int, newSort: Sort, nameGenerator: NameGenerator) {
-    // TODO generate constant names instead? Probably better for other methodsThe fundamental operations on maps are similar to those on sets. They are summarized in the following table and fall into the following categories:
-    // TODO custom name generator with a number we can specify?
     val intToConstants: Map[Int, Var] = Range(min, max+1).map(value => {
-        val newName = f"_${value}_${newSort.name}"
+        val newName = nameGenerator.freshName(f"_${value}_${newSort.name}")
         (value -> Var(newName))
     }).toMap
 
@@ -41,209 +39,130 @@ class IntegerToSortConverter(min: Int, max: Int, newSort: Sort, nameGenerator: N
     private def inRange(x: Int): Boolean = {
         return (x >= min && x <= max)
     }
-    
-    // A map from a builtin to a definition for it
-    var convertableFunctions: Map[BuiltinFunction, (FunctionDefinition)] = {
-        
-        // x is left value, y is right value, z is result value
-        // Looks through all cases, with default
-        def generateFunctionBody2(op: (Int, Int) => Int, checkDivZero: Boolean = false): Term = {
-            var body: Term = intToConstants(min)
-            for (left <- Range(min, max+1)) {
-                for (right <- Range(min, max+1)) {
-                    if (checkDivZero && right == 0){
-                        // skip because div by zero should be an overlfow
+
+    // wraps a term in a fromInt cast. Simplifies out toInts
+    def fromInt(arg: Term): Term = arg match {
+        case App(fname, inner) if fname == castToInt.name && inner.size == 1 => inner(0)
+        case _ => App(castFromInt.name, arg)
+    }
+
+    // wraps a term in a toInt cast. Simplifies out fromInts
+    def toInt(arg: Term): Term = arg match {
+        case App(fname, inner) if fname == castFromInt.name && inner.size == 1 => inner(0)
+        case _ => App(castToInt.name, arg)
+    }
+
+    // TODO apply to problem state
+
+    // Change the sort of annotated vars of sort int to the newSort. other sorts are unchanged
+    def replaceAvarSort(av: AnnotatedVar): AnnotatedVar = av.sort match {
+        case IntSort => AnnotatedVar(av.variable, newSort)
+        case _ => av
+    }
+
+    def castArgsForUninterpreted(args: Seq[Term], paramSorts: Seq[Sort]): Seq[Term] = {
+        args.zip(paramSorts).map({case (arg, sort) => sort match {
+            case `newSort` => fromInt(arg)
+            case _ => arg
+        }})
+    }
+
+
+    def replaceInt(term: Term, sig: Signature): Term = term match {
+        // Quantifiers: replace Int sorts with newSort and recurse
+        case Forall(vars, body) => {
+            val newVars = vars map replaceAvarSort
+            val newSig = sig.withConstantDeclarations(newVars)
+            Forall(newVars, replaceInt(body, newSig))
+        }
+        case Exists(vars, body) => {
+            val newVars = vars map replaceAvarSort
+            val newSig = sig.withConstantDeclarations(newVars)
+            Exists(newVars, replaceInt(body, newSig))
+        }
+
+        case App(fname, args) => {
+            val recursedArgs = args.map(replaceInt(_, sig))
+            sig.queryFunction(fname) match {
+                // Cast int args to 
+                case None => Errors.Internal.preconditionFailed(f"No function with name ${fname} in signature")
+                case Some(Left(FuncDecl(_, paramSorts, resultSort))) => {
+                    // Cast if parameter sort is the new sort
+                    val castArgs = castArgsForUninterpreted(recursedArgs, paramSorts)
+                    
+                    val appWithCastArgs = App(fname, castArgs)
+
+                    if (resultSort == newSort) {
+                        toInt(appWithCastArgs)
                     } else {
-                        val result = op(left, right)
-                        if (inRange(result)){
-                            body = IfThenElse(And(Eq(x, intToConstants(left)), Eq(y, intToConstants(right))), intToConstants(result), body)
-                        }
+                        appWithCastArgs
                     }
                 }
-            }
-            body
-        }
-        // TODO clean this up somehow? Make the interpreted function cleaner?
-        // Could we just use the cast to int for comparisons?
-        // x is left value, y is right value
-        def generateComparisonBody(op: (Int,Int) => Boolean): Term = {
-            var body: Term = Bottom // default to false?
-            for (left <- Range(min, max+1)){
-                for (right <- Range(min, max+1)){
-                    val result =  if (op(left, right)) {Top} else {Bottom}
-                    // if x and y are what we checked, then it's the proper result.
-                    body = IfThenElse(And(Eq(x, intToConstants(left)), Eq(y, intToConstants(right))), result, body)
+                // Just recurse, don't cast
+                case Some(Right(FunctionDefinition(_, params, resultSort, _))) => {
+                    App(fname, recursedArgs)
                 }
             }
-            body
         }
 
-        def generateFunctionDefinition2(nameBase: String, op: (Int, Int) => Int, checkDivZero: Boolean = false): FunctionDefinition = {
-            FunctionDefinition(nameGenerator.freshName(nameBase), axy, newSort, generateFunctionBody2(op, checkDivZero))
-        }
-        Map[BuiltinFunction, FunctionDefinition](
-            IntPlus -> generateFunctionDefinition2("+", _+_),
-            IntSub -> generateFunctionDefinition2("-", _-_),
-            IntMult -> generateFunctionDefinition2("*", _*_),
-            IntDiv -> generateFunctionDefinition2("/", _/_, true),
-            // TODO MORE
-        )
-    }
-    // A function to create a function declaration for a version of the builtin function using the new sort
-    def createFuncDecl(func: BuiltinFunction): FuncDecl = {
-        ???
-    }
-    // The term is the new term, the boolean is true if we have done a substitution
-    class IntegerToSortVisitor extends TermVisitor[(Term, Boolean)] {
-
-        override def visitTop(): (Term, Boolean) = (Top, false)
-
-        override def visitBottom(): (Term, Boolean) = (Bottom, false)
-
-        override def visitVar(term: Var): (Term, Boolean) = (term, false)
-
-        override def visitEnumValue(term: EnumValue): (Term, Boolean) = (term, false)
-
-        override def visitDomainElement(term: DomainElement): (Term, Boolean) = (term, false)
-
-        override def visitNot(term: Not): (Term, Boolean) = {
-            // Substitute into the body
-            val (newBody, substituted) = visit(term.body)
-            (Not(newBody), substituted)
-        }
-
-        def visitAndList(term: AndList) = {
-            // Substitute each of the bodies
-            val (newBodies, substituted) = term.arguments.map(visit).unzip
-            // do we want to raise substituted? We are now
-            val didSub = substituted.exists(x => x) // Just looks for one that is true
-            (AndList(newBodies), didSub)
-        }
-
-        override def visitOrList(term: OrList): (Term, Boolean) = {
-            // Substitute each of the bodies
-            val (newBodies, substituted) = term.arguments.map(visit).unzip
-            // do we want to raise substituted? We are now
-            val didSub = substituted.exists(x => x) // Just looks for one that is true
-            (OrList(newBodies), didSub)
-        }
-
-        override def visitDistinct(term: Distinct): (Term, Boolean) = {
-            // Substitute each of the bodies
-            val (newBodies, substituted) = term.arguments.map(visit).unzip
-            // do we want to raise substituted? We are now
-            val didSub = substituted.exists(x => x) // Just looks for one that is true
-            (Distinct(newBodies), didSub)
-        }
-
-        override def visitImplication(term: Implication): (Term, Boolean) = {
-            val (newLeft, subLeft) = visit(term.left)
-            val (newRight, subRight) = visit(term.right)
-            (Implication(newLeft, newRight), subLeft || subRight)
-        }
-
-        override def visitIff(term: Iff): (Term, Boolean) = {
-            val (newLeft, subLeft) = visit(term.left)
-            val (newRight, subRight) = visit(term.right)
-            (Iff(newLeft, newRight), subLeft || subRight)
-        }
-
-        override def visitEq(term: Eq): (Term, Boolean) = {
-            val (newLeft, subLeft) = visit(term.left)
-            val (newRight, subRight) = visit(term.right)
-            (Eq(newLeft, newRight), subLeft || subRight)
-        }
-
-        override def visitApp(term: App): (Term, Boolean) = {
-            // Functions need to be type-changed ahead of time
-            val (arguments, substituted) = term.arguments.map(visit).unzip
-            val anySubstituted = substituted.exists(x => x)
-            (App(term.functionName, arguments), anySubstituted)
-        }
-
-        override def visitBuiltinApp(term: BuiltinApp): (Term, Boolean) = {
-            // Substitute internals
-            val (newArgs, argsSubstituted) = term.arguments.map(visit).unzip
-            val anyArgSubstituted = argsSubstituted.exists(x => x)
-
-            // If already defined, just do that
-            convertedFunctions.get(term.function) match {
-                case Some(newFunctionDef) => return (App(newFunctionDef.name, newArgs), true)
-                case None => None
-            }
-
-            // Check if we redefine the function
-            if (convertableFunctions isDefinedAt term.function) {
-                var newDecl = createFuncDecl(term.function)
-                (App(newDecl.name, newArgs), true)
-            } else {
-                // Do not convert
-                (BuiltinApp(term.function, newArgs), anyArgSubstituted)
-            }
-
-
-        }
-
-        override def visitExists(term: Exists): (Term, Boolean) = {
-            var changedTypes = false
-            val newVars = term.vars.map(_ match {
-                case AnnotatedVar(v, IntSort) => {
-                    changedTypes = true
-                    AnnotatedVar(v, newSort)
+        // Closure arguments to uninterpreted functions must be cast like normal calls
+        // We don't have to check result sort here
+        case Closure(functionName, arg1, arg2, fixedArgs) => {
+            val args = arg1 +: arg2 +: fixedArgs
+            val recursedArgs = args.map(replaceInt(_, sig))
+            sig.queryFunction(functionName) match {
+                case None => Errors.Internal.preconditionFailed(f"Cannot find sort for function |${functionName}|")
+                case Some(Right(_)) => Closure(functionName, recursedArgs(0), recursedArgs(1), recursedArgs.tail.tail)
+                case Some(Left(FuncDecl(_, paramSorts, _))) => {
+                    val castArgs = castArgsForUninterpreted(recursedArgs, paramSorts)
+                    Closure(functionName, castArgs(0), castArgs(1), castArgs.tail.tail)
                 }
-                case avar => avar
-            })
-            var (newBody, substitutedBody) = visit(term.body)
-            (Exists(newVars, newBody), changedTypes || substitutedBody)
+            }
         }
 
-        override def visitForall(term: Forall): (Term, Boolean) = {
-            var changedTypes = false
-            val newVars = term.vars.map(_ match {
-                case AnnotatedVar(v, IntSort) => {
-                    changedTypes = true
-                    AnnotatedVar(v, newSort)
+        case ReflexiveClosure(functionName, arg1, arg2, fixedArgs) => {
+            val args = arg1 +: arg2 +: fixedArgs
+            val recursedArgs = args.map(replaceInt(_, sig))
+            sig.queryFunction(functionName) match {
+                case None => Errors.Internal.preconditionFailed(f"Cannot find sort for function |${functionName}|")
+                case Some(Right(_)) => ReflexiveClosure(functionName, recursedArgs(0), recursedArgs(1), recursedArgs.tail.tail)
+                case Some(Left(FuncDecl(_, paramSorts, _))) => {
+                    val castArgs = castArgsForUninterpreted(recursedArgs, paramSorts)
+                    ReflexiveClosure(functionName, castArgs(0), castArgs(1), castArgs.tail.tail)
                 }
-                case avar => avar
-            })
-            var (newBody, substitutedBody) = visit(term.body)
-            (Forall(newVars, newBody), changedTypes || substitutedBody)
+            }
         }
 
-        override def visitExists2ndOrder(term: Exists2ndOrder): (Term, Boolean) = ???
-
-        override def visitForall2ndOrder(term: Forall2ndOrder): (Term, Boolean) = ???
-
-        override def visitIntegerLiteral(term: IntegerLiteral): (Term, Boolean) = (intToConstants(term.value), true)
-
-        override def visitBitVectorLiteral(term: BitVectorLiteral): (Term, Boolean) = (term, false)
-
-        override def visitSetCardinality(term: SetCardinality): (Term, Boolean) = ???
-        
-        override def visitIfThenElse(term: IfThenElse): (Term, Boolean) = {
-            val (newConditional, substitutedConditional) = visit(term.condition)
-            val (newIfTrue, substitutedTrue) = visit(term.ifTrue)
-            val (newIfFalse, substitutedFalse) = visit(term.ifFalse)
-            val substituted = substitutedConditional || substitutedTrue || substitutedFalse
-            (IfThenElse(newConditional, newIfTrue, newIfFalse), substituted)
+        // Cast to int if this is newSort. Otherwise leave unchanged.
+        case v: Var => {
+            sig.queryConstantDeclaration(v) match {
+                case None => Errors.Internal.preconditionFailed(f"IntegerToSort can't find sort of variable ${v.name}")
+                case Some(sort) => sort match {
+                    case `newSort` => toInt(v)
+                    case _ => term
+                }
+            }
         }
 
-        override def visitClosure(term: Closure): (Term, Boolean) = {
-            // Substitute each of the bodies
-            val (newArguments, substituted) = term.allArguments.map(visit).unzip
-            // do we want to raise substituted? We are now
-            val didSub = substituted.exists(x => x) // Just looks for one that is true
-            (Term.mkClosure(term.functionName, newArguments), didSub)
+        // Pass recursion through
+        case AndList(arguments) => AndList(arguments.map(replaceInt(_, sig)))
+        case OrList(arguments) => OrList(arguments.map(replaceInt(_, sig)))
+        case BuiltinApp(function, arguments) => BuiltinApp(function, arguments.map(replaceInt(_, sig)))
+        case Distinct(arguments) => Distinct(arguments.map(replaceInt(_, sig)))
+        case Eq(left, right) => Eq(replaceInt(left, sig), replaceInt(right, sig))
+        case Iff(left, right) => Iff(replaceInt(left, sig), replaceInt(right, sig))
+        case Implication(left, right) => Implication(replaceInt(left, sig), replaceInt(right, sig))
+        case Not(body) => Not(replaceInt(body, sig))
+        case IfThenElse(c, t, f) => {
+            IfThenElse(replaceInt(c, sig), replaceInt(t, sig), replaceInt(f, sig))
         }
 
-        override def visitReflexiveClosure(term: ReflexiveClosure): (Term, Boolean) = {
-            // Substitute each of the bodies
-            val (newArguments, substituted) = term.allArguments.map(visit).unzip
-            // do we want to raise substituted? We are now
-            val didSub = substituted.exists(x => x) // Just looks for one that is true
-            (Term.mkClosure(term.functionName, newArguments), didSub)
-        }
+        // Other leaf terms are unchanged
+        case _: LeafTerm => term
 
-        
+        // Should be gone by now
+        case _: Exists2ndOrder => Errors.Internal.impossibleState("IntegerToSortConverter does not support 2nd order functions.")
+        case _: Forall2ndOrder => Errors.Internal.impossibleState("IntegerToSortConverter does not support 2nd order functions.")
     }
+
 }
